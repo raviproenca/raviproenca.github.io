e15b85753e93a51f39e0c91f577a8fd62f27174d
Ravi Ribeiro Proença <raviproenca@gmail.com>
2025-09-26 15:01:52 -0300

    Updating Quasar

--- app-quasar/src/components/UserModalComponent.vue
+++ app-quasar/src/components/UserModalComponent.vue
@@ -1,9 +1,9 @@
 <template>
   <q-card class="shadow-4 bg-white q-pa-md">
     <q-card-section>
-      <p>Carlos Alberto</p>
-      <p>carlos@example.com</p>
-      <p>Leitor</p>
+      <p>{{ userData.name }}</p>
+      <p>{{ userData.email }}</p>
+      <p>{{ userData.role }}</p>
     </q-card-section>
 
     <q-btn
@@ -17,3 +17,19 @@
     ></q-btn>
   </q-card>
 </template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import { useUsersStore } from 'src/stores/users-store'
+import { storeToRefs } from 'pinia'
+
+const userString = localStorage.getItem('user')
+const loginUser = ref(userString ? JSON.parse(userString) : null)
+
+const usersStore = useUsersStore()
+const { users } = storeToRefs(usersStore)
+
+const userData = computed(() => {
+  return users.value.find((user) => user.email === loginUser.value.email)
+})
+</script>
