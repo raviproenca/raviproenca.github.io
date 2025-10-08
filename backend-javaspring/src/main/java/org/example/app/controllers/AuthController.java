9aa4abde956ed32b0871cf28d8cf40745fe0538e
Ravi Ribeiro Proença <raviproenca@gmail.com>
2025-10-08 15:20:49 -0300

    Starting Java Spring Boot

---
+++ backend-javaspring/src/main/java/org/example/app/controllers/AuthController.java
@@ -0,0 +1,16 @@
+package org.example.app.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
---
+++ backend-javaspring/src/main/java/org/example/app/models/dtos/AuthDTO.java
@@ -0,0 +1,9 @@
+package org.example.app.models.dtos;
+
+import lombok.Data;
+
+@Data
+public class AuthDTO {
+    private String email;
+    private String password;
+}
