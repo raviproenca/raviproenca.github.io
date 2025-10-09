feb9e778cf6ba9f212af58d6cc05f3e2bc203539
Ravi Ribeiro Proença <raviproenca@gmail.com>
2025-10-09 12:49:08 -0300

    Updating Spring Boot

--- backend-javaspring/src/main/java/org/example/app/controllers/AuthController.java
+++ backend-javaspring/src/main/java/org/example/app/controllers/AuthController.java
@@ -1,6 +1,12 @@
 package org.example.app.controllers;
 
+import org.example.app.models.dtos.AuthDTO;
+import org.example.app.models.entities.UserEntity;
+import org.example.app.services.AuthService;
+import org.example.app.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService service;
+
     @PostMapping("/login")
-    public String hello() {
-        return "Hello World";
+    public UserEntity LoginController(@RequestBody AuthDTO authDTO) {
+        return service.loginService(authDTO);
     }
 
 }
--- backend-javaspring/src/main/java/org/example/app/services/AuthService.java
+++ backend-javaspring/src/main/java/org/example/app/services/AuthService.java
@@ -4,9 +4,25 @@ import lombok.RequiredArgsConstructor;
 import org.example.app.models.entities.UserEntity;
 import org.example.app.repositories.AuthRepository;
 import org.example.app.models.dtos.AuthDTO;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Service
+
 @RequiredArgsConstructor
+@Service
 public class AuthService {
+
+    private final AuthRepository authRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserEntity loginService(AuthDTO login) {
+        UserEntity userEntity = authRepository.findByEmail(login.getEmail())
+                .orElseThrow(() -> new RuntimeException("Usuário com o email " + login.getEmail() + " não encontrado."));
+
+        if (!passwordEncoder.matches(login.getPassword(), userEntity.getPassword())) {
+            throw new RuntimeException("Senha inválida");
+        }
+
+        return userEntity;
+    }
 }
